56f79426dd3000e693f95b6dfe4f3575f726bc63
Hazel Mapuranga (1000125469) <MAPUHN1@student.op.ac.nz>
2024-05-21 14:49:02 +1200

    added VS

---
+++ Zombies Survival/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Zombies_Survival
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
