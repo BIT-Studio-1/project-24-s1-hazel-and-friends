f71a096f8ed3958c97e5db592a84cccab7b24083
Caleb Chin (1000128574) <CHINC5@student.op.ac.nz>
2024-05-23 10:49:47 +1200

    menu code

--- Zombies Survival/Program.cs
+++ Zombies Survival/Program.cs
@@ -1,10 +1,78 @@
-﻿namespace Zombies_Survival
+﻿namespace Skeleton_Program
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int tasks;
+            do
+
+            {
+                Console.Clear();
+                Console.WriteLine("The Walking Jail: \n1 Play game \n2 Instructions \n3 Options \n4 Credits \n0 Exit game");
+                tasks = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (tasks)
+                {
+                    case 1:
+
+                        task1();
+                        break;
+
+                    case 2:
+
+                        task2();
+                        break;
+
+                    case 3:
+
+                        task3();
+                        break;
+                    case 4:
+
+                        task4();
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Exit");
+                        break;
+
+                }
+
+            } while (tasks != 0);
+
         }
+        public static void task1()
+        {
+            Console.WriteLine("Task 1");
+            Console.WriteLine("Press any key to return");
+            Console.ReadLine();
+        }
+        public static void task2()
+        {
+            Console.WriteLine("Task 2");
+            Console.WriteLine("Press any key to return");
+            Console.ReadLine();
+        }
+        public static void task3()
+        {
+            Console.WriteLine("Task 3");
+            Console.WriteLine("Press any key to return");
+            Console.ReadLine();
+        }
+        public static void task4()
+        {
+            Console.WriteLine("Task 4");
+            Console.WriteLine("Press any key to return");
+            Console.ReadLine();
+
+
+        }
+
+
     }
+
+
 }
+
+
