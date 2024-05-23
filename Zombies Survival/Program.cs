0077a7a187448cd85074f5339b34c67f86a0aa91
Keith Dacosta (1000112606) <DACOKF1@student.op.ac.nz>
2024-05-23 12:09:05 +1200

    Room 5

--- Zombies Survival/Program.cs
+++ Zombies Survival/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Skeleton_Program
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Skeleton_Program
 {
     internal class Program
     {
@@ -33,6 +36,10 @@
                         task4();
                         break;
 
+                    case 5:
+                        task5();
+                        break;
+
                     case 0:
                         Console.WriteLine("Exit");
                         break;
@@ -44,9 +51,7 @@
         }
         public static void task1()
         {
-            Console.WriteLine("Task 1");
-            Console.WriteLine("Press any key to return");
-            Console.ReadLine();
+
         }
         public static void task2()
         {
@@ -69,7 +74,52 @@
 
         }
 
+        public static void task5(){
+            String temp;
+            Console.WriteLine("Do you wish to enter room 5?");
+            temp = Console.ReadLine().ToLower();
+            if (temp.Equals("y")) {
+                Console.Clear();
+                room5();
+            }else if (temp.Equals("n"))
+            {
+                Console.WriteLine("You stayed");
+                Main();
+            }
+            
+            }
 
+    public static void room5()
+        {
+            List<string> items = new List<string>();        //Just trying something
+            int temp;
+            Console.Write("Room 5");
+            Console.WriteLine("Clear, box with resources, bandages or etc...");
+            Console.WriteLine("1- Select box\n2-bandages\n3- etc");
+            temp = Convert.ToInt32(Console.ReadLine());
+
+            switch (temp) { 
+                case 1:
+                    Console.WriteLine("You select the box");
+                    break;
+                    case 2:
+                    Console.WriteLine("You select bandages");
+                    items.Add("2 bandages");
+                    items.Add("3 mags of 9mm bullets");
+                    Console.WriteLine($"You now have ....");
+
+                    break;
+                    case 3:
+                    Console.WriteLine("idk bro");
+                    break;
+
+                default:
+                    Console.WriteLine("Choose a number");
+                    break;
+                    
+            }
+            Console.ReadLine();
+        }
     }
 
 
